aa3cd14da7d70707e46335f7a988d66251e8f259
Neverrest <c.tangvasinkul@gmail.com>
2021-05-31 20:42:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@
 
   window.fbAsyncInit = function() {
     FB.init({
-      appId      : '178096127280582',
+      appId      : '[YOUR APP ID]',
       cookie     : true,                     // Enable cookies to allow the server to access the session.
       xfbml      : true,                     // Parse social plugins on this webpage.
       version    : 'v10.0'                   // Use this Graph API version for this call.
