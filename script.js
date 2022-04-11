368d5869aeb8bd4ee284bbbe41201965abce36e0
Chitsanupong <first30952@gmail.com>
2022-04-11 14:33:17 +0700

    feat: uncomment

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-// function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-//     console.log('statusChangeCallback');
-//     console.log(response);                   // The current login status of the person.
-//     if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-//       testAPI();
-//     } else {                                 // Not logged into your webpage or we are unable to tell.
-//       document.getElementById('status').innerHTML = 'Please log ' + 'into this webpage.';
-//     }
-//   }
+function statusChangeCallback(response) {
+  // Called with the results from FB.getLoginStatus().
+  console.log('statusChangeCallback');
+  console.log(response); // The current login status of the person.
+  if (response.status === 'connected') {
+    // Logged into your webpage and Facebook.
+    testAPI();
+  } else {
+    // Not logged into your webpage or we are unable to tell.
+    document.getElementById('status').innerHTML =
+      'Please log ' + 'into this webpage.';
+  }
+}
 
-//   function checkLoginState() {               // Called when a person is finished with the Login Button.
-//     FB.getLoginStatus(function(response) {   // See the onlogin handler
-//       statusChangeCallback(response);
-//     });
-//   }
+function checkLoginState() {
+  // Called when a person is finished with the Login Button.
+  FB.getLoginStatus(function (response) {
+    // See the onlogin handler
+    statusChangeCallback(response);
+  });
+}
 
-// function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-//   console.log('Welcome!  Fetching your information.... ');
-//   FB.api('/me', function(response) {
-//     console.log('Successful login for: ' + response.name);
-//     document.getElementById('status').innerHTML =
-//       'Thanks for logging in, ' + response.name + '!';
-//   });
-// }
+function testAPI() {
+  // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function (response) {
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
+}
 
 window.fbAsyncInit = function () {
   FB.init({
@@ -31,9 +38,10 @@ window.fbAsyncInit = function () {
     version: 'v10.0', // Use this Graph API version for this call.
   });
 
-  // FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-  //   statusChangeCallback(response);        // Returns the login status.
-  // });
+  FB.getLoginStatus(function (response) {
+    // Called after the JS SDK has been initialized.
+    statusChangeCallback(response); // Returns the login status.
+  });
 };
 
 function GetProfile() {
