c62e4d9f2d0c8d8307780a162c68cf5a6d072de8
Chitsanupong <first30952@gmail.com>
2022-04-12 19:33:30 +0700

    feat: remove my app id

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function testAPI() {
 
 window.fbAsyncInit = function () {
   FB.init({
-    appId: '1251308928608500',
+    appId: '[YOUR_APP_ID]',
     cookie: true, // Enable cookies to allow the server to access the session.
     xfbml: true, // Parse social plugins on this webpage.
     version: 'v10.0', // Use this Graph API version for this call.
